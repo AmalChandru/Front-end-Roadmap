54bac76ec61c1e8f270327ca7d4b2ca0446d8936
AmalChandru <amaltathva@gmail.com>
2021-06-12 15:20:08 +0530

    D1-R

---
+++ 3.React/react-complete-guide/src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <h2>Title</h2>
+      </div>
+      <div>Amount</div>
+    </div>
+  );
+}
+export default ExpenseItem;
