6f97738b44ede03c5026bdec9da8c9269af6e1c9
AmalChandru <amaltathva@gmail.com>
2021-06-13 18:24:02 +0530

    D3-R

--- 3.React/react-complete-guide/src/components/ExpenseItem.js
+++ 3.React/react-complete-guide/src/components/ExpenseItem.js
@@ -1,4 +1,5 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 //props are basically objects consist of attributes to our component
 //key value pair
 function ExpenseItem(props) {
@@ -6,17 +7,13 @@ function ExpenseItem(props) {
   // const expenseDate = new Date(2021, 2, 28);
   // const expenseTitle = "Car Insurance";
   // const expenseAmount = 294.67;
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
+  // const month = props.date.toLocaleString("en-US", { month: "long" });
+  // const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  // const year = props.date.getFullYear();
 
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
