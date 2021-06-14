ed05bd130f5381ac45459b16a902013b6f14f0d1
AmalChandru <amaltathva@gmail.com>
2021-06-14 13:48:01 +0530

    D4-R

--- 3.React/react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ 3.React/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,9 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
 //props are basically objects consist of attributes to our component
 //key value pair
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   //Date object new for constructor
   // const expenseDate = new Date(2021, 2, 28);
   // const expenseTitle = "Car Insurance";
@@ -12,13 +13,13 @@ function ExpenseItem(props) {
   // const year = props.date.getFullYear();
 
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
-}
+};
 export default ExpenseItem;
