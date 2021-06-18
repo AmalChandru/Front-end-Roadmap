5dd55dfcbc3394b0870d7ed918509e762ef92fe8
AmalChandru <amaltathva@gmail.com>
2021-06-18 10:28:15 +0530

    D7-R

--- 3.React/react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ 3.React/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,30 +1,16 @@
-import { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 //props are basically objects consist of attributes to our component
 //key value pair
 const ExpenseItem = (props) => {
-  //Date object new for constructor
-  // const expenseDate = new Date(2021, 2, 28);
-  // const expenseTitle = "Car Insurance";
-  // const expenseAmount = 294.67;
-  // const month = props.date.toLocaleString("en-US", { month: "long" });
-  // const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  // const year = props.date.getFullYear();
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("Updated!");
-  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
