9a907d34ed277e52acf8dd70d361ead40b865a33
AmalChandru <amaltathva@gmail.com>
2021-06-23 19:12:35 +0530

    D10 R ExpenseTracker Finished

--- 3.React/react-complete-guide/src/components/Expenses/Expenses.js
+++ 3.React/react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
-import ExpensesFilter from "./ExpensesFilter";
+
 import Card from "../UI/Card";
-import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+import "./Expenses.css";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
-  //function for obtaining selected year from ExpensesFilter.js
-  const filterChangeHandler = (enteredYear) => {
-    setFilteredYear(enteredYear);
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
   };
+
   const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
@@ -17,10 +20,15 @@ const Expenses = (props) => {
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter onChangeFilter={filterChangeHandler} />
-        <ExpensesList items={filteredExpenses}></ExpensesList>
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
 };
+
 export default Expenses;
